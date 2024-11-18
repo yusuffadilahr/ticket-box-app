e688b43dd2d39b1cc6eeb6360e59f53b7a42301f
Bobby <bobbysudiharto3@gmail.com>
2024-11-18 15:01:01 +0700

    feat: bobby_features_review

--- apps/web/src/utils/midtrans.ts
+++ apps/web/src/utils/midtrans.ts
@@ -18,16 +18,16 @@
 //     return window.snap;
 // }
 
-// export default async function loadSnap() {
-//     const snapSrc = 'https://app.sandbox.midtrans.com/snap/snap.js';
-//     if (!document.querySelector(`script[src="${snapSrc}"]`)) {
-//         const script = document.createElement('script');
-//         script.src = snapSrc;
-//         script.async = true;
-//         document.body.appendChild(script);
-//         await new Promise((resolve) => {
-//             script.onload = resolve;
-//         });
-//     }
-//     return window.snap;
-// }
+export default async function loadSnap() {
+    const snapSrc = 'https://app.sandbox.midtrans.com/snap/snap.js';
+    if (!document.querySelector(`script[src="${snapSrc}"]`)) {
+        const script = document.createElement('script');
+        script.src = snapSrc;
+        script.async = true;
+        document.body.appendChild(script);
+        await new Promise((resolve) => {
+            script.onload = resolve;
+        });
+    }
+    return window.snap;
+}
